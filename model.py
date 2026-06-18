206ac1108fdde1b0efe1d314532cf3962cda774a
Satyam1942 <126737709+Satyam1942@users.noreply.github.com>
2026-06-18 10:11:20 +0530

    Step 37: apply_residual_add_and_norm

--- model.py
+++ model.py
@@ -321,8 +321,12 @@ def normalize_and_scale_with_gamma_beta(x, gamma, beta, eps=1e-5):
    x_cap = (x-mean) / torch.sqrt(var + eps)
    return gamma*x_cap + beta
 
-# Step 37 - apply_residual_add_and_norm (not yet solved)
-# TODO: implement
+# Step 37 - apply_residual_add_and_norm
+import torch
+
+def apply_residual_add_and_norm(residual_input, sublayer_output, gamma, beta, eps=1e-5):
+    vec = residual_input+sublayer_output
+    return normalize_and_scale_with_gamma_beta(vec, gamma, beta, eps)
 
 # Step 38 - apply_dropout_with_keep_mask (not yet solved)
 # TODO: implement
